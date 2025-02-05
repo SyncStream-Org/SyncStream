799c0a726379824c04776b71fb895bc99954b47f
Nathaniel Bielanski <nbielans@purdue.edu>
2025-02-05 12:26:32 -0500

    eslint with jest server

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginJest from 'eslint-plugin-jest';
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +10,19 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {    
+    // update this to match your test files
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  }
 ];
