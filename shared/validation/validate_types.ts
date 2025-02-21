a0ddf7d04b26b6cd753281b1c2dd881ec326a4cf
Nathaniel Bielanski <nbielans@purdue.edu>
2025-02-20 22:08:12 -0500

    admin controllers

--- shared/validation/validate_types.ts
+++ shared/validation/validate_types.ts
@@ -9,6 +9,15 @@ export function isUserDataAuth(userData: any): userData is { username:string, pa
             && typeof userData.password === "string"
 }
 
+export function isUserDataCreate(userData: any): userData is { username:string, email:string, password:string, admin:boolean, displayName:string } {
+    return !!userData 
+            && typeof userData.username === "string"
+            && typeof userData.email === "string"
+            && typeof userData.password === "string"
+            && typeof userData.admin === "boolean"
+            && typeof userData.displayName === "string"
+}
+
 export function isUserUpdateDate(userUpdateData: any): boolean {
     return !!userUpdateData &&
             (
