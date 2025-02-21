d405f91a92f114fb01c62abd08df620d596c7b76
Nathaniel Bielanski <nbielans@purdue.edu>
2025-02-20 21:50:14 -0500

    admin middleware

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import * as controller from "../controllers/admin.controller";
 
+import { adminMiddleware } from "src/middleware/admin";
+
 const router = Router();
+
+router.use(adminMiddleware)
 
 router.post("/user/create", controller.createUser);
 router.delete("/user/delete", controller.deleteUser);
