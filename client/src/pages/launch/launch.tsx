5addf9f44a50ccc2eff91a003ae8f125ff8039ba
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2025-02-22 01:03:37 +0000

    Remove dev bypass now that admin user added

--- client/src/pages/launch/launch.tsx
+++ client/src/pages/launch/launch.tsx
@@ -187,15 +187,6 @@ class Launch extends React.Component<
             Submit
           </button>
         </form>
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={() => {
-            this.props.navigate('/home');
-          }}
-        >
-          DEV BYPASS
-        </button>
       </>
     );
   }
