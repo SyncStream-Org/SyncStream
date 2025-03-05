e5c30637eded0cb819bde426f1532068f0a3ebff
Alden Parker <ajparker1401@gmail.com>
2025-03-05 17:01:10 -0500

    fix delete user bug

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,11 +1,11 @@
-import User from '../models/users';
-import Room from '../models/rooms';
-import RoomUser from '../models/roomUsers';
+import User from "../models/users";
+import Room from "../models/rooms";
+import RoomUser from "../models/roomUsers";
 
-import { RoomUserAttributes, RoomUserPermissions } from 'room-types';
-import { UserAttributes } from 'user-types';
+import { RoomUserAttributes, RoomUserPermissions } from "room-types";
+import { UserAttributes } from "user-types";
 
-import * as auth from '../utils/auth';
+import * as auth from "../utils/auth";
 
 class UserService {
   public async getUserByUsername(username: string): Promise<User | null> {
@@ -15,10 +15,10 @@ class UserService {
   }
 
   public async createUser(user: UserAttributes): Promise<User> {
-    if (await this.getUserByUsername(user.username) != null) {
-      throw new Error('createUser: User already exists');
+    if ((await this.getUserByUsername(user.username)) != null) {
+      throw new Error("createUser: User already exists");
     }
-    
+
     const hashedPassword = await auth.hashPassword(user.password);
     user.password = hashedPassword;
 
@@ -26,11 +26,13 @@ class UserService {
   }
 
   public async deleteUser(user: User): Promise<void> {
-    if (await this.getUserByUsername(user.username) == null) {
-      throw new Error('deleteUser: User not found');
+    if ((await this.getUserByUsername(user.username)) == null) {
+      throw new Error("deleteUser: User not found");
     }
 
     const rooms = await this.getUserRooms(user, false, true);
+
+    console.log(rooms);
 
     // TODO: delete owned rooms once room service is implemented
     for (const room of rooms) {
@@ -43,9 +45,14 @@ class UserService {
     await user.destroy();
   }
 
-  public async updateUser(user: User, newPassword?: string, newDisplayName?: string, newEmail?: string): Promise<User> {
-    if (await this.getUserByUsername(user.username) == null) {
-      throw new Error('updateUser: User not found');
+  public async updateUser(
+    user: User,
+    newPassword?: string,
+    newDisplayName?: string,
+    newEmail?: string,
+  ): Promise<User> {
+    if ((await this.getUserByUsername(user.username)) == null) {
+      throw new Error("updateUser: User not found");
     }
 
     if (newPassword) {
@@ -62,27 +69,39 @@ class UserService {
     return await user.save();
   }
 
-  public async getUserRooms(user: User, isOwner?: boolean, isMember?: boolean): Promise<Room[]> {
-    if (await this.getUserByUsername(user.username) == null) {
-      throw new Error('getUserRooms: User not found');
+  public async getUserRooms(
+    user: User,
+    isOwner?: boolean,
+    isMember?: boolean,
+  ): Promise<Room[]> {
+    if ((await this.getUserByUsername(user.username)) == null) {
+      throw new Error("getUserRooms: User not found");
     }
 
     isOwner = isOwner || false;
     isMember = isMember || false;
-    
+
     const rooms: Room[] = [];
     if (isOwner) {
-      const ownedRooms = await Room.findAll({ where: { roomOwner: user.username } });
+      const ownedRooms = await Room.findAll({
+        where: { roomOwner: user.username },
+      });
       if (ownedRooms) {
         rooms.push(...ownedRooms);
       }
     }
 
-    const joinedRoomLinks = await RoomUser.findAll({ where: { username: user.username, isMember: isMember } });
-    if (joinedRoomLinks) { 
-      const joinedRoomIDs = joinedRoomLinks.map((room: RoomUser) => room.roomID);
-      const joinedRooms = await Room.findAll({ where: { roomID: { in: joinedRoomIDs } } });
-      if (joinedRooms) { 
+    const joinedRoomLinks = await RoomUser.findAll({
+      where: { username: user.username, isMember: isMember },
+    });
+    if (joinedRoomLinks && joinedRoomLinks.length > 0) {
+      const joinedRoomIDs = joinedRoomLinks.map(
+        (room: RoomUser) => room.roomID,
+      );
+      const joinedRooms = await Room.findAll({
+        where: { roomID: { in: joinedRoomIDs } },
+      });
+      if (joinedRooms) {
         rooms.push(...joinedRooms);
       }
     }
@@ -90,34 +109,46 @@ class UserService {
     return rooms;
   }
 
-  public async getRoomUser(roomID: string, username: string): Promise<RoomUser | null> {
-    if (await Room.findOne({ where: { roomID: roomID } }) == null) {
-      throw new Error('getRoomUser: Room not found');
+  public async getRoomUser(
+    roomID: string,
+    username: string,
+  ): Promise<RoomUser | null> {
+    if ((await Room.findOne({ where: { roomID: roomID } })) == null) {
+      throw new Error("getRoomUser: Room not found");
     }
 
-    const roomUser = await RoomUser.findOne({ where: { roomID, username: username } });
-    
+    const roomUser = await RoomUser.findOne({
+      where: { roomID, username: username },
+    });
+
     return roomUser;
   }
 
-  public async createRoomUser(roomID: string, roomUser: RoomUserAttributes): Promise<RoomUser> {
-    if (await this.getRoomUser(roomID, roomUser.username) != null) {
-      throw new Error('createRoomUser: User already in room');
+  public async createRoomUser(
+    roomID: string,
+    roomUser: RoomUserAttributes,
+  ): Promise<RoomUser> {
+    if ((await this.getRoomUser(roomID, roomUser.username)) != null) {
+      throw new Error("createRoomUser: User already in room");
     }
 
     return await RoomUser.create(roomUser);
   }
 
   public async removeRoomUser(roomUser: RoomUser): Promise<void> {
-    if (await this.getRoomUser(roomUser.roomID, roomUser.username) == null) {
-      throw new Error('removeRoomUser: User not in room');
+    if ((await this.getRoomUser(roomUser.roomID, roomUser.username)) == null) {
+      throw new Error("removeRoomUser: User not in room");
     }
     return await roomUser.destroy();
   }
 
-  public async updateRoomUser(roomUser: RoomUser, newPermissions?: RoomUserPermissions, newStatus?: boolean): Promise<RoomUser> {
-    if (await this.getRoomUser(roomUser.roomID, roomUser.username) == null) {
-      throw new Error('updateRoomUser: User not in room');
+  public async updateRoomUser(
+    roomUser: RoomUser,
+    newPermissions?: RoomUserPermissions,
+    newStatus?: boolean,
+  ): Promise<RoomUser> {
+    if ((await this.getRoomUser(roomUser.roomID, roomUser.username)) == null) {
+      throw new Error("updateRoomUser: User not in room");
     }
 
     if (newPermissions !== undefined) {
