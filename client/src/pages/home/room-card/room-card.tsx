08f48415b7c1bd82bd6ce853b94db59c71000c20
gtnolan <gtnolan23@gmail.com>
2025-03-31 11:57:52 -0400

    join room before navigating to room page

--- client/src/pages/home/room-card/room-card.tsx
+++ client/src/pages/home/room-card/room-card.tsx
@@ -3,6 +3,8 @@ import './room-card.css';
 
 import { Types } from 'syncstream-sharedlib';
 import { NavigateFunction } from 'react-router-dom';
+import * as api from '../../../api';
+import { SuccessState } from '@/api';
 
 interface Props {
   roomData: Types.RoomData;
@@ -20,13 +22,28 @@ export default class RoomCard extends React.Component<Props, State> {
 
   // TODO: Make card display if you are part of a room officially or not if the user is admin
   render() {
+    const handleJoinRoom = (event: React.SyntheticEvent) => {
+      api.User.joinRoomPresence(this.props.roomData.roomID!).then(
+        (success: SuccessState) => {
+          if (
+            success === api.SuccessState.ERROR ||
+            success === api.SuccessState.FAIL
+          ) {
+            window.electron.ipcRenderer.invokeFunction('show-message-box', {
+              title: 'Error',
+              message: 'Unable to join room at this time.',
+            });
+          } else {
+            this.props.navigate(`/room/${this.props.roomData.roomID}`);
+          }
+        },
+      );
+    };
     // ---- RENDER BLOCK ----
     return (
       <div
         className="dark:bg-gray-800 p-6 rounded-lg shadow-md"
-        onClick={() => {
-          this.props.navigate(`/room/${this.props.roomData.roomID}`);
-        }}
+        onClick={handleJoinRoom}
       >
         <h3 className="text-xl font-bold mb-4">
           {this.props.roomData.roomName}
