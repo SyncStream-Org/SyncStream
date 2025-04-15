9a89f675569597c1c26e6ae0e942741a0d961328
gtnolan <gtnolan23@gmail.com>
2025-04-14 21:02:10 -0400

    editor ui done

--- client/src/pages/room/editor/toolbar.tsx
+++ client/src/pages/room/editor/toolbar.tsx
@@ -18,47 +18,51 @@ export function Toolbar({ editor }: ToolbarProps) {
 
   return (
     <div className="shrink-0 overflow-x-auto border-b border-border p-2">
-      <div className="flex w-max items-center gap-px">
-        <SectionOne editor={editor} activeLevels={[1, 2, 3]} />
+      <div className="flex w-full items-center gap-px">
+        <div className="flex items-center gap-px flex-1">
+          <SectionOne editor={editor} activeLevels={[1, 2, 3]} />
+
+          <Separator orientation="vertical" className="mr-2 h-4" />
+
+          <SectionTwo
+            editor={editor}
+            activeActions={[
+              'italic',
+              'bold',
+              'underline',
+              'code',
+              'strikethrough',
+              'clearFormatting',
+            ]}
+            mainActionCount={5}
+          />
+
+          <Separator orientation="vertical" className="mr-2 h-4" />
+
+          <SectionThree editor={editor} />
+
+          <Separator orientation="vertical" className="mr-2 h-4" />
+
+          <SectionFour
+            editor={editor}
+            activeActions={['bulletList', 'orderedList']}
+            mainActionCount={2}
+          />
+
+          <Separator orientation="vertical" className="mr-2 h-4" />
+
+          <SectionFive
+            editor={editor}
+            activeActions={['blockquote', 'codeBlock', 'horizontalRule']}
+            mainActionCount={3}
+          />
+        </div>
 
         <Separator orientation="vertical" className="mr-2 h-4" />
 
-        <SectionTwo
-          editor={editor}
-          activeActions={[
-            'italic',
-            'bold',
-            'underline',
-            'code',
-            'strikethrough',
-            'clearFormatting',
-          ]}
-          mainActionCount={5}
-        />
-
-        <Separator orientation="vertical" className="mr-2 h-4" />
-
-        <SectionThree editor={editor} />
-
-        <Separator orientation="vertical" className="mr-2 h-4" />
-
-        <SectionFour
-          editor={editor}
-          activeActions={['bulletList', 'orderedList']}
-          mainActionCount={2}
-        />
-
-        <Separator orientation="vertical" className="mr-2 h-4" />
-
-        <SectionFive
-          editor={editor}
-          activeActions={['blockquote', 'codeBlock', 'horizontalRule']}
-          mainActionCount={3}
-        />
-
-        <Separator orientation="vertical" className="mr-2 h-4" />
-
-        <SectionSix editor={editor} />
+        <div className="ml-auto">
+          <SectionSix editor={editor} />
+        </div>
       </div>
     </div>
   );
