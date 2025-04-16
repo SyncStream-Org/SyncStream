950c81b23e02bd29c367e247e1e23820822b96df
Nathaniel Bielanski <nbielans@purdue.edu>
2025-04-16 13:25:32 -0400

    open api /api endpoint

--- server/src/routes/misc.routes.ts
+++ server/src/routes/misc.routes.ts
@@ -5,5 +5,6 @@ import { ErrorCatcher } from "../middleware/errorCatcher";
 const router = Router();
 
 router.post("/echo", ErrorCatcher(controller.echo));
+router.get("/api", ErrorCatcher(controller.getAPI));
 
 export default router;
