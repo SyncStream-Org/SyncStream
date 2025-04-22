1221aa9f61d030a3e6ba74951c961917844e9bcb
Nathaniel Bielanski <nbielans@purdue.edu>
2025-04-22 11:40:43 -0400

    rate limiter for authMiddleware

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,6 +7,13 @@ import roomsRouter from "./rooms.routes";
 
 import { authMiddleware } from "../middleware/setup"
 import { ErrorCatcher } from "../middleware/errorCatcher";
+import rateLimit from "express-rate-limit";
+
+// rate limit to 1000 rq per 15min
+const limiter = rateLimit({
+    windowMs: 15*60*1000,
+    max: 1000
+}); 
 
 const router = Router();
 
@@ -14,6 +21,7 @@ router.use("/", miscRouter);
 router.use("/user", userRouter);
 
 router.use(ErrorCatcher(authMiddleware));
+router.use(limiter, authMiddleware);
 
 router.use("/admin", adminRouter);
 router.use("/rooms", roomsRouter);
