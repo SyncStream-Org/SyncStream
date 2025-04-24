8695a0df395db259abaa4b92f02e261dee7d34e2
Alden Parker <ajparker1401@gmail.com>
2025-04-24 12:53:48 -0400

    server update

--- server/src/websockets/routes.ts
+++ server/src/websockets/routes.ts
@@ -2,6 +2,7 @@ import { Router, Application } from "express";
 import expressWs from "express-ws";
 import wsDoc from "./controllers/doc";
 import wsAudioCalls from "./controllers/audioCalls";
+import wsVideoStreams from "./controllers/videoStreams";
 import { wsAuth, wsPresence } from "./wsMiddleware";
 
 const routerWs = Router();
@@ -9,5 +10,11 @@ expressWs(routerWs as unknown as Application);
 
 routerWs.ws("/rooms/:roomID/doc/:docID", wsAuth, wsPresence, wsDoc);
 routerWs.ws("/rooms/:roomID/voice/:channel", wsAuth, wsPresence, wsAudioCalls);
+routerWs.ws(
+  "/rooms/:roomID/stream/:channel",
+  wsAuth,
+  wsPresence,
+  wsVideoStreams,
+);
 
 export default routerWs;
