1504b3e337171e8739851284476753b21bf49a97
gtnolan <gtnolan23@gmail.com>
2025-04-28 14:07:08 -0400

    update language settings

--- client/src/pages/settings/language.tsx
+++ client/src/pages/settings/language.tsx
@@ -1,38 +1,49 @@
-import React from 'react';
-import './settings.css';
-
-import SessionState from '../../utilities/session-state';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
 import Localize, { Language, LanguageArray } from '../../utilities/localize';
-import PrimarySelect from '../../components/selects/primary-select';
 
 interface Props {
   forceUpdate: () => void;
 }
 
-interface State {}
+function LanguageSettings({ forceUpdate }: Props) {
+  // Grab localize engine
+  const localize = Localize.getInstance().localize();
 
-export default class LanguageSettings extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+  return (
+    <div className="flex items-center gap-4">
+      <Label
+        htmlFor="language-select"
+        className="whitespace-nowrap text-gray-600 dark:text-gray-300"
+      >
+        {localize.settingsPage.language.languageChange || 'Display Language:'}
+      </Label>
 
-    this.state = {};
-  }
-
-  render() {
-    // Grab localize engine
-    const localize = Localize.getInstance().localize();
-
-    // ---- RENDER BLOCK ----
-    return (
-      <PrimarySelect
-        label={localize.settingsPage.language.languageChange}
-        categories={LanguageArray}
-        defaultCategory={Localize.getInstance().currentLanguage}
-        onChange={(category) => {
-          Localize.getInstance().currentLanguage = category as Language;
-          this.props.forceUpdate();
+      <Select
+        onValueChange={(language) => {
+          Localize.getInstance().currentLanguage = language as Language;
+          forceUpdate();
         }}
-      />
-    );
-  }
+      >
+        <SelectTrigger id="language-select" className="w-[180px]">
+          <SelectValue placeholder={Localize.getInstance().currentLanguage} />
+        </SelectTrigger>
+        <SelectContent>
+          {LanguageArray.map((language) => (
+            <SelectItem key={language} value={language}>
+              {language}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
+
+export default LanguageSettings;
